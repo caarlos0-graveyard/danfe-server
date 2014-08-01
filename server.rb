5da4afcf7288ee16d9b4033e1ad91998c67b84ea
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-08-01 07:36:44 -0300

    exception handling

--- server.rb
+++ server.rb
@@ -6,7 +6,14 @@ configure { set :server, :puma }
 
 post '/' do
   file = params[:file]
-  filename = "/tmp/#{Time.now.to_f * 1000}_#{file[:filename]}.pdf"
-  RubyDanfe.generate(filename, file[:tempfile])
-  send_file filename, type: :pdf
+  begin
+    filename = "/tmp/#{Time.now.to_f * 1000}_#{file[:filename]}.pdf"
+    RubyDanfe.generate(filename, file[:tempfile])
+    send_file filename, type: :pdf
+  rescue => e
+    message = "Failed to convert file: #{file[:filename]}"
+    status 500
+    body message
+    raise e, message
+  end
 end
